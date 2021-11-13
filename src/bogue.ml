167f4e168b21cb7ab1cd605e564440eb04e46f1e
Sebastian Willenbrink <sewi@linux.fritz.box>
2021-11-13 18:02:37 +0100

    Restructure repository

---
+++ src/bogue.ml
@@ -0,0 +1,47 @@
+(** BOGUE *)
+(** A GUI Library for Ocaml, using SDL2 *)
+
+(* this is file bogue.ml *)
+
+(** Vu Ngoc San, December 2013 -- now *)
+
+module Main = Main
+module Bogue = Main
+module Layout = Layout
+module Widget = Widget
+module Utf8 = Utf8
+module Utils = Utils
+module Chain = Chain
+module Theme = Theme
+module Time = Time
+module Avar = Avar
+module Var = Var
+module Tvar = Tvar
+module Timeout = Timeout
+module Trigger =  Trigger
+module Mixer = Mixer
+module Sync = Sync
+module Draw = Draw
+module Mouse = Mouse
+module Selection = Selection
+module Image = Image
+module Style = Style
+module Box = Box
+module Label = Label
+module Button = Button
+module Slider = Slider
+module Check = Check
+module Text_display = Text_Display
+module Update = Update
+module Text_input = Text_Input
+module Space = Space
+module Print = Print
+module Snapshot = Snapshot
+module Long_list = Long_List
+module Tabs = Tabs
+module Popup = Popup
+module Menu = Menu
+module Select = Select
+module Radiolist = Radiolist
+module Table = Table
+module Window = Window
